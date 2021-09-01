851861861cf3c771676124fd82444337fc18126e
Charlie <cmthomlinson@gmail.com>
2021-09-01 20:57:37 +1200

    commit

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -6,17 +6,14 @@ const Picture = ({floor_id, que_id}) => {
     
     const pic_name = "/img/" + Questiondata[floor_id][que_id]['img']['img_name']
 
-    if (pic_name === "/img/Quakestar_280px.png") {
-        return ""
-    }
-    else {
-        return  (
-            <div className="picture">
-                <Image src={process.env.PUBLIC_URL + pic_name} rounded />
-            </div>
+    
+    return  (
+        <div className="picture">
+            <Image src={process.env.PUBLIC_URL + '/img/Walls.png'}/>
+        </div>
 
-        )
-    }
+    )
+    
 }
 
 export default Picture;
