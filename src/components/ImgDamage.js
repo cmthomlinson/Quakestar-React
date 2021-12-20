50e02f1ec7322a7805cc0dfba003948688c279c5
Charlie <cmthomlinson@gmail.com>
2021-12-20 19:31:41 +1300

    commit

--- src/components/ImgDamage.js
+++ src/components/ImgDamage.js
@@ -5,17 +5,17 @@ const ImgDamage = ({damage}) => {
     if (damage > 80) {
         return <Image src={require('../img/0.png')}/>
     }
-    if (damage < 80) {
-        return <Image src={require('../img/1.png')}/>
-    }
-    if (damage < 40) {
-        return <Image src={require('../img/2.png')}/>
+    if (damage < 10) {
+        return <Image src={require('../img/4.png')}/>
     }
     if (damage < 20) {
         return <Image src={require('../img/3.png')}/>
     }
-    if (damage < 10) {
-        return <Image src={require('../img/4.png')}/>
+    if (damage < 40) {
+        return <Image src={require('../img/2.png')}/>
+    }
+    if (damage < 80) {
+        return <Image src={require('../img/1.png')}/>
     }
     else {
         return <Image src={require('../img/5.png')}/>
